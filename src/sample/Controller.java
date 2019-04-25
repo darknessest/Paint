20cce614a7268404d7d59c7e151608a66d195ed3
Darknessest <darknessest>
2019-04-25 14:26:46 +0800

    Improved drawing of basic figures. Code clean-up TODO: Undo/Redo

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,5 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
@@ -10,8 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
+import javafx.scene.input.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,25 +16,42 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        gcB = canvas_main.getGraphicsContext2D();
+        //two-layered system
+        canvBack = canvas_main.getGraphicsContext2D();
+        canvVisual = canvas_visual.getGraphicsContext2D();
+
+
+        lastState = canvas_main.getGraphicsContext2D();
+
 
         lineSizeSlide.setMin(1);
         lineSizeSlide.setMax(50);
 
-        isFilled = true;
-        curDrawingCircle = false;
         curDrawingBrush = true;
+        curDrawingLine = false;
+        curDrawingRectangle = false;
+        curDrawingOval = false;
     }
 
-    private GraphicsContext gcB, gcF;
+    //************************************
+    //***           VARIABLES          ***
+    //************************************
+    private GraphicsContext canvBack, canvVisual;
     private double startX, startY, lastX, lastY, oldX, oldY;
-    private boolean dragging, isFilled;
-    private boolean curDrawingCircle, curDrawingBrush;
+    private boolean curDrawingBrush, curDrawingLine, curDrawingRectangle, curDrawingOval, curErasing;
+    //For ctrl+z
+    private GraphicsContext lastState;
 
+    private KeyCombination keyCombCtrZ = new KeyCodeCombination(KeyCode.Z, KeyCombination.SHORTCUT_DOWN);
+    private KeyCombination keyCombCtrY = new KeyCodeCombination(KeyCode.Y, KeyCombination.SHORTCUT_DOWN);
+
+    //************************************
+    //***            BUTTONS           ***
+    //************************************
     @FXML
-    Canvas canvas_main;
+    Canvas canvas_main, canvas_visual;
     @FXML
-    Button drawCircle, drawBrush, drawSquare, drawTriangle, drawOval;
+    Button BrushButton, LineButton, RectangleButton, OvalButton, ClearButton;
     @FXML
     Slider lineSizeSlide;
     @FXML
@@ -45,47 +59,240 @@ public class Controller implements Initializable {
     @FXML
     CheckBox drawFilled;
 
+    //*****************************************
+    //***          Keyboard controls        ***
+    //*****************************************
+
+    //TODO undo/redo
     @FXML
-    private void clearCanvas(ActionEvent e) {
-        gcB.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
-        gcF.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+    public void KeyboardPressed(KeyEvent e) {
+        if (keyCombCtrZ.match(e) || keyCombCtrY.match(e)) {
+            canvBack = lastState;
+            System.out.println("Ctrl+Z/Y is pressed");
+        }
     }
 
     @FXML
+    public void KeyboardReleased(KeyEvent e) {
+        System.out.println("released");
+    }
+
+    //*****************************************
+    //***       Mouse controls start        ***
+    //*****************************************
+
+    @FXML
     public void pressedOnCanvas(MouseEvent e) {
+//        lastState = canvBack;
+//        System.out.println("added on stack");
+
         this.startX = e.getX();
         this.startY = e.getY();
         this.oldX = e.getX();
         this.oldY = e.getY();
-        System.out.println("pressed : " + this.lastX + " - " + this.lastY);
-
     }
 
     @FXML
     public void mouseReleased(MouseEvent e) {
-        dragging = false;
+//        canvBack.save();
+//        System.out.println("added on stack");
+
+        canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+        if (curDrawingLine)
+            drawLine();
+        if (curDrawingRectangle)
+            drawRectangle();
+        if (curDrawingOval)
+            drawOval();
     }
 
     @FXML
     public void mouseDragged(MouseEvent e) {
         this.lastX = e.getX();
         this.lastY = e.getY();
-        System.out.println("dragged : " + this.lastX + " - " + this.lastY);
         if (curDrawingBrush)
-            freeDrawing();
+            drawBrush();
+        if (curDrawingLine)
+            drawLineVisual();
+        if (curDrawingOval)
+            drawOvalVisual();
+        if (curDrawingRectangle)
+            drawRectVisual();
+        if (curErasing)
+            erasing();
     }
 
     @FXML
-    public void onMouseExitedListener(MouseEvent event) {
-        System.out.println("Mouse has disappeared");
+    public void onMouseExitedListener(MouseEvent e) {
+//        System.out.println("Mouse has disappeared");
     }
 
-    private void freeDrawing() {
-        gcB.setLineWidth(lineSizeSlide.getValue());
-        gcB.setStroke(colorPicker.getValue());
-        gcB.strokeLine(oldX, oldY, lastX, lastY);
+
+    //*****************************************
+    //***       Buttons controls start      ***
+    //*****************************************
+
+    @FXML
+    public void pressBrush() {
+        curDrawingBrush = true;
+        curDrawingLine = false;
+        curDrawingRectangle = false;
+        curDrawingOval = false;
+        curErasing = false;
+    }
+
+    @FXML
+    public void pressLine() {
+        curDrawingBrush = false;
+        curDrawingLine = true;
+        curDrawingRectangle = false;
+        curDrawingOval = false;
+        curErasing = false;
+    }
+
+    @FXML
+    public void pressRectangle() {
+        curDrawingBrush = false;
+        curDrawingLine = false;
+        curDrawingRectangle = true;
+        curDrawingOval = false;
+        curErasing = false;
+    }
+
+    @FXML
+    public void pressOval() {
+        curDrawingBrush = false;
+        curDrawingLine = false;
+        curDrawingRectangle = false;
+        curDrawingOval = true;
+        curErasing = false;
+    }
+
+    @FXML
+    public void pressEraser() {
+        curDrawingBrush = false;
+        curDrawingLine = false;
+        curDrawingRectangle = false;
+        curDrawingOval = false;
+        curErasing = true;
+        System.out.println("erasing");
+    }
+
+    @FXML
+    private void clearCanvas() {
+        canvBack.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+        canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+    }
+
+
+    //*****************************************
+    //***       Drawing functions start     ***
+    //*****************************************
+
+    private void drawBrush() {
+        canvBack.setLineWidth(lineSizeSlide.getValue());
+        canvBack.setStroke(colorPicker.getValue());
+        canvBack.strokeLine(oldX, oldY, lastX, lastY);
         oldX = lastX;
         oldY = lastY;
     }
 
+    private void drawLine() {
+        canvBack.setLineWidth(lineSizeSlide.getValue());
+        canvBack.setStroke(colorPicker.getValue());
+        canvBack.strokeLine(startX, startY, lastX, lastY);
+    }
+
+    private void drawRectangle() {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        canvBack.setLineWidth(lineSizeSlide.getValue());
+
+        double cornerX = (wh > 0 ? startX : startX + wh);
+        double cornerY = (hg > 0 ? startY : startY + hg);
+
+        if (drawFilled.isSelected()) {
+            canvBack.setFill(colorPicker.getValue());
+            canvBack.fillRect(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        } else {
+            canvBack.setStroke(colorPicker.getValue());
+            canvBack.strokeRect(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        }
+    }
+
+    private void drawOval() {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        canvBack.setLineWidth(lineSizeSlide.getValue());
+
+        double cornerX = (wh > 0 ? startX : startX + wh);
+        double cornerY = (hg > 0 ? startY : startY + hg);
+
+        if (drawFilled.isSelected()) {
+            canvBack.setFill(colorPicker.getValue());
+            canvBack.fillOval(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        } else {
+            canvBack.setStroke(colorPicker.getValue());
+            canvBack.strokeOval(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        }
+    }
+
+    private void erasing() {
+        canvBack.clearRect(lastX, lastY, 10, 10);
+        canvVisual.clearRect(lastX, lastY, 10, 10);
+        oldX = lastX;
+        oldY = lastY;
+        System.out.printf("%f - %f : %f - %f\n", lastX, lastY, lastX + 5, lastY + 5);
+    }
+
+
+    //*****************************************
+    //***       Drawing visuals start       ***
+    //*****************************************
+
+    private void drawLineVisual() {
+        canvVisual.setLineWidth(lineSizeSlide.getValue());
+        canvVisual.setStroke(colorPicker.getValue());
+        canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+        canvVisual.strokeLine(startX, startY, lastX, lastY);
+    }
+
+    private void drawOvalVisual() {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        canvVisual.setLineWidth(lineSizeSlide.getValue());
+
+        double cornerX = (wh > 0 ? startX : startX + wh);
+        double cornerY = (hg > 0 ? startY : startY + hg);
+
+        if (drawFilled.isSelected()) {
+            canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+            canvVisual.setFill(colorPicker.getValue());
+            canvVisual.fillOval(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        } else {
+            canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+            canvVisual.setStroke(colorPicker.getValue());
+            canvVisual.strokeOval(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        }
+
+    }
+
+    private void drawRectVisual() {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        canvVisual.setLineWidth(lineSizeSlide.getValue());
+
+        double cornerX = (wh > 0 ? startX : startX + wh);
+        double cornerY = (hg > 0 ? startY : startY + hg);
+
+        if (drawFilled.isSelected()) {
+            canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+            canvVisual.setFill(colorPicker.getValue());
+            canvVisual.fillRect(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        } else {
+            canvVisual.clearRect(0, 0, canvas_main.getWidth(), canvas_main.getHeight());
+            canvVisual.setStroke(colorPicker.getValue());
+            canvVisual.strokeRect(cornerX, cornerY, Math.abs(wh), Math.abs(hg));
+        }
+    }
 }
